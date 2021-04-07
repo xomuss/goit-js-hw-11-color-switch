1c12210f48ce39c4f8c8a4f38c2a92f63444e36e
Alexander Khomenko <72103285+xomuss@users.noreply.github.com>
2021-04-07 16:30:47 +0300

    finish

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+
+const min = 0;
+const max = colors.length - 1;
+let timerActiv = false;
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  if (timerActiv) {
+    return;
+  }
+  timerActiv = true;
+  timerId = setInterval(() => {
+    onStartBtnClick();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});
+
+function onStartBtnClick() {
+  const colorIndex = colors[randomIntegerFromInterval(min, max)];
+  document.body.style = `background: ${colorIndex}`;
+}
+
+function onStopBtnClick() {}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
